77f1fb9ed832c39da933173de472fb151afa492d
Matheo-13 <matheo.taillandier@gmail.com>
2026-04-01 11:34:39 +0200

    small update for pip install directly

--- setup.py
+++ setup.py
@@ -14,16 +14,16 @@ with open("requirements.txt", "r") as fh:
 
 setuptools.setup(
     name="trajectory_planning_helpers",
-    version="2.0.8",
+    version="2.0.9",
     url="https://github.com/EPFL-RT-Driverless/trajectory_planning_helpers",
     author="Alexander Heilmeier, Tim Stahl, Fabian Christ, Tudor Oancea",
     description="Useful functions used for path and trajectory planning",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    packages=setuptools.find_packages("trajectory_planning_helpers"),
+    packages=["trajectory_planning_helpers"],
     install_requires=requirements,
     classifiers=[
-        "Development Status :: 5 - Production/Stable"
+        "Development Status :: 5 - Production/Stable",
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)",
         "Operating System :: OS Independent",
